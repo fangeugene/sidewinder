88b68ecd0e9a39f36af015ecdbea6a8a67c0453a
Eugene Fang <fang.eugene@gmail.com>
2014-02-03 22:33:19 +0000

    watchdog, enable/disable, joystick rotation

--- beaglebone/testing/public/script.js
+++ beaglebone/testing/public/script.js
@@ -1,4 +1,5 @@
 var ws = new MozWebSocket('ws://' + window.location.hostname + ':8081');
+var enable = false;
 
 ws.onopen = function() {
   // Web Socket is connected, send data using send()
@@ -18,6 +19,16 @@ ws.onmessage = function (evt) {
     $('#module0power').attr('r', Math.log(parseInt(split_msg[1])));
     $('#module1power').attr('r', Math.log(parseInt(split_msg[3])));
     $('#module2power').attr('r', Math.log(parseInt(split_msg[5])));
+  } else if (split_msg[0] == '9') {
+    if (split_msg[1] == '10') {
+      $('#enable-status').css('background-color', 'green');
+      $('#enable-button').text('Disable!');
+      enable = true;
+    } else {
+      $('#enable-status').css('background-color', 'red');
+      $('#enable-button').text('Enable!');
+      enable = false;
+    }
   }
 };
 ws.onclose = function() {
@@ -26,38 +37,46 @@ ws.onclose = function() {
 };
 
 $(document).ready(function(){
-  setInterval(function() { ws.send('9'); }, 100);  // watchdog
+  setInterval(function() { ws.send('9'); }, 20);  // watchdog
 
-  var ledon = false;
-  $('#button1').click(function() {
-    if (ledon) {
+  $('#enable-button').click(function() {
+    if (enable) {
       ws.send('0 0');
-      ledon = false;
     } else {
       ws.send('0 1');
-      ledon = true;
     }
   });
 
   var theta = 0;
-  var last_translation_msg = '';
-  function send_translation(x, y) {
+  var last_movement_msg = '';
+  function send_movement(x, y, w) {
     x *= -100;
     y *= -100;
+    w *= -100;
+
+    if (w < 25 && w > -25) {
+      w = 0;
+    }
 
     var r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-    theta = Math.atan(y/x);
-    if (x <= 0) {
-      theta += Math.PI;
+    if (Math.round(x) == 0 && Math.round(y) == 0) {
+      r = 1;
+    } else {
+      theta = Math.atan(y/x);
+      if (x <= 0) {
+        theta += Math.PI;
+      }
+      if (theta > Math.PI) {
+        theta -= 2 * Math.PI;
+      }
+      theta = theta * 180 / Math.PI;
     }
-    if (theta > Math.PI) {
-      theta -= 2 * Math.PI;
-    }
-    theta = theta * 180 / Math.PI;
-    var msg = '1 ' + pad(parseInt(r/2), 3) +  ' ' + pad(parseInt(theta+500), 3);
-    if (last_translation_msg != msg) {
+
+    var msg = '1 ' + pad(parseInt(r), 3) +  ' ' + pad(parseInt(theta+500), 3) + ' ' + pad(parseInt(w+500), 3);
+
+    if (last_movement_msg != msg) {
       ws.send(msg);
-      last_translation_msg = msg;
+      last_movement_msg = msg;
     }
   }
 
@@ -77,9 +96,9 @@ $(document).ready(function(){
 
   setInterval(function() {
     if (controller == null) {
-      send_translation(0, 0);
+      send_movement(0, 0, 0);
     } else {
-      send_translation(controller.axes[1], controller.axes[0]);
+      send_movement(controller.axes[1], controller.axes[0], controller.axes[3]);
     }
   }, 10);
 });

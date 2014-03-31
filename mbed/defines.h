39b738c4ac0f896a533ba970b1ad8d2900f1a867
Eugene Fang <fang.eugene@gmail.com>
2014-03-30 22:39:41 -0700

    helper fns

--- mbed/defines.h
+++ mbed/defines.h
@@ -3,4 +3,15 @@
 
 const int kMaxMsgSize = 100;
 
+// Helper Functions
+template <typename T>
+inline T min(T x,T y) { return (x < y) ? x : y; }
+template <typename T>
+inline T max(T x,T y) { return (x > y) ? x : y; }
+template <typename T>
+inline T abs(T x) { return (x < 0) ? -x : x; }
+template <typename T>
+inline T constrain(T x, T min, T max){
+  return x > max ? max : (x < min ? min : x); }
+
 #endif

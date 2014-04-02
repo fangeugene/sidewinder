b46fedcd0c923493230e16e9f36a93a20d3cc5e6
Eugene Fang <fang.eugene@gmail.com>
2014-04-02 00:03:09 -0700

    add MotorDriver class

---
+++ mbed/MotorDriver/MotorDriver.cpp
@@ -0,0 +1,21 @@
+#include "MotorDriver.h"
+
+MotorDriver::MotorDriver(PinName pwm, PinName fwd, PinName rev):
+                           _pwm(pwm), _fwd(fwd), _rev(rev) {
+  _pwm.period_us(kPwmPeriodUs);
+  _pwm.pulsewidth_us(0);
+  _fwd = 0;
+  _rev = 0;
+}
+
+void MotorDriver::setPower(int power) {
+	power = constrain(power, -100, 100);
+	_pwm.pulsewidth_us(abs(power * kPwmPeriodUs / 100));
+	if (power >= 0) {
+		_fwd = 1;
+		_rev = 0;
+	} else {
+		_fwd = 0;
+		_rev = 1;
+	}
+}

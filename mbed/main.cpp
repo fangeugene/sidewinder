44db6ff9c9bc039cdb3c778ee4efb5d4bef01c12
Eugene Fang <fang.eugene@gmail.com>
2014-04-10 15:15:57 -0700

    remove temp testing code

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -21,22 +21,6 @@ MotorDriver m2_drive(PTB0, PTA14, PTD7);  // 1
 SwerveDrive* swervedrive;
 IMU* imu;
 
-// // Temp for testing
-// volatile unsigned int i = 0;
-// unsigned int n = 0;
-// void encoder_isr_simulator() {
-//   i += 1;
-//   wait_us(2);
-// }
-
-// void encoder_simulator_printer_thread(void const *args) {
-//   while (true) {
-//     pc.printf("Count: %d, %d\r\n", i, n);
-//     Thread::wait(10000);
-//   }
-// }
-// // End temp for testing
-
  void get_msg_thread(void const *args) {
    char msg[kMaxMsgSize];
    while (true) {
@@ -88,12 +72,6 @@ int main() {
                                 m0_steer, m0_drive,
                                 m1_steer, m1_drive,
                                 m2_steer, m2_drive);
-
-  // Temp for testing
-  //Ticker t;
-  //t.attach_us(&encoder_isr_simulator, 30);
-  //Thread encoderSimThread(encoder_simulator_printer_thread);
-  // End temp for testing
 
   // IMU setup
   // imu::gyro_calibrate();
